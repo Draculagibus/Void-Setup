ede6b0c261ef66992ba43d8ef9fe17bbd9737eeb
Draculagibus <146088933+Draculagibus@users.noreply.github.com>
2025-09-19 12:02:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -151,7 +151,8 @@ echo "User rights added!"
 echo "Install Bibata Cursor..."
 wget -nc https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Classic.tar.xz
 tar -xf Bibata-Modern-Classic.tar.xz
-mv Bibata-Modern-Classic/ /usr/share/icons/
+mkdir ~/.icons/
+mv Bibata-Modern-Classic/ ~/.icons/
 echo "Bibata cursor installed successfully!"
 
 echo "Apply Dot files..."
